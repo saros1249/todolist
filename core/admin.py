1d45f2028fa81a328f766a58910ccaea4f7b1963
saros1249 <saros1249@gmail.com>
2023-03-01 20:19:10 +0800

    yaml, гнужны дораотки

--- core/admin.py
+++ core/admin.py
@@ -1,16 +1,3 @@
 from django.contrib import admin
 
-from .models import User
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
-
-@admin.register(User)
-class UserAdmin(BaseUserAdmin):
-    list_display = ['username', 'email', 'first_name', 'last_name']
-    list_filter = ['is_staff', 'is_active', 'is_superuser']
-    search_fields = ['username', 'email', 'first_name', 'last_name']
-    exclude = ('password',)
-    readonly_fields = ('date_joined', 'last_login',)
-
-
-
+# Register your models here.
